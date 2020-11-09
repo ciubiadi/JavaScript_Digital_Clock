bb8940e379ef2622d338f66922f81e580ce55415
Adrian Albert Ciubucciu <ciubucciu.adrianalbert@gmail.com>
2020-11-09 19:15:36 +0200

    The user can choose if he wants the Clasic style or the Smooth style for the clock. However in the next update she should be able to choose clicking on buttons how many times he wants

--- script.js
+++ script.js
@@ -1,36 +1,81 @@
+
+let choice = window.prompt("Write \"clasic\" or \"smooth\"");
+
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-var date = new Date();
+if(choice=="smooth") {
 
-// Getting the real time
-let hr = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
 
-// converting the real seconds, minutes and hours into degrees
-let hourPosition = (hr*360/12)+(min*(360/60)/12);
 
-// The basic method will be min*360/60, but we want to make a smooth transition so we will add the seconds
-let minPosition = (min*360/60)+(sec*(360/60)/60);
+  var date = new Date();
 
-let secPosition = sec*360/60;
+  // Getting the real time
+  let hr = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
 
-function runTheClock() {
+  // converting the real seconds, minutes and hours into degrees
+  let hourPosition = (hr*360/12)+(min*(360/60)/12);
 
-  //We grab the existing hour position. It moves the distance of 1 hour so it will be (360deg/12)/3600seconds
-  hourPosition = hourPosition+(3/360);
+  // The basic method will be min*360/60, but we want to make a smooth transition so we will add the seconds
+  let minPosition = (min*360/60)+(sec*(360/60)/60);
 
-//We calculate the position of the minutes. It will be minPosition+(1/60)*6
-  minPosition = minPosition+(6/60);
+  let secPosition = sec*360/60;
 
-  secPosition = secPosition+6;
+  function runTheClock() {
 
-  HOURHAND.style.transform = "rotate(" + hourPosition + "deg)";
-  MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-  SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+    //We grab the existing hour position. It moves the distance of 1 hour so it will be (360deg/12)/3600seconds
+    hourPosition = hourPosition+(3/360);
+
+  //We calculate the position of the minutes. It will be minPosition+(1/60)*6
+    minPosition = minPosition+(6/60);
+
+    secPosition = secPosition+6;
+
+    HOURHAND.style.transform = "rotate(" + hourPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+
+  }
+
+  var interval = setInterval(runTheClock, 1000);
+
+} else if(choice=="clasic") {
+
+  const HOURHAND = document.querySelector("#hour");
+  const MINUTEHAND = document.querySelector("#minute");
+  const SECONDHAND = document.querySelector("#second");
+
+  function runTheClock() {
+
+    var date = new Date();
+
+    // Getting the real time
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+    // converting the real seconds, minutes and hours into degrees
+    let hourPosition = (hr*360/12)+(min*(360/60)/12);
+
+    // The basic method will be min*360/60, but we want to make a smooth transition so we will add the seconds
+    let minPosition = (min*360/60)+(sec*(360/60)/60);
+
+    let secPosition = sec*360/60;
+
+
+    HOURHAND.style.transform = "rotate(" + hourPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+
+  }
+
+  var interval = setInterval(runTheClock, 1000);
+
+  document.querySelector('#hour').style.transition = "none";
+  document.querySelector('#minute').style.transition = "none";
+  document.querySelector('#second').style.transition = "none";
 
 }
-
-var interval = setInterval(runTheClock, 1000);
